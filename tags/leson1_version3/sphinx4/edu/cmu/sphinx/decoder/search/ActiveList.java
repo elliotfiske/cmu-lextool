99924b4615baae1740e768f976e410bb9b346f91
nobody <nobody@94700074-3cef-4d97-a70e-9c8c206c02f5>
2004-01-16 13:07:41 +0000

    This commit was manufactured by cvs2svn to create tag 'leson1_version3'.

---
+++ tags/leson1_version3/sphinx4/edu/cmu/sphinx/decoder/search/ActiveList.java
@@ -0,0 +1,203 @@
+
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+package edu.cmu.sphinx.decoder.search;
+import edu.cmu.sphinx.util.SphinxProperties;
+import edu.cmu.sphinx.decoder.search.Token;
+import edu.cmu.sphinx.decoder.scorer.Scoreable;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * An active list is maintained as a sorted list
+ *
+ * Note that all scores are represented in LogMath logbase
+ */
+public interface ActiveList {
+
+
+    /**
+     * Prefix string for all ActiveList properties.
+     */
+    public final static String PROP_PREFIX
+        = "edu.cmu.sphinx.decoder.search.ActiveList.";
+
+
+    /**
+     * property that sets the desired (or target) size for this
+     * active list.  This is sometimes referred to as the beam size
+     */
+    public final static String PROP_ABSOLUTE_BEAM_WIDTH  =
+        PROP_PREFIX + "absoluteBeamWidth";
+
+    /**
+     * The default value for the PROP_ABSOLUTE_BEAM_WIDTH property
+     */
+    public final static int PROP_ABSOLUTE_BEAM_WIDTH_DEFAULT = 2000;
+
+    /**
+     * Property that sets the minimum score relative to the maximum
+     * score in the list for pruning.  Tokens with a score less than
+     * relativeBeamWidth * maximumScore will be pruned from the list
+     */
+
+    public final static String PROP_RELATIVE_BEAM_WIDTH
+        = PROP_PREFIX + "relativeBeamWidth";
+
+    /**
+     * The default value for the PROP_RELATIVE_BEAM_WIDTH property
+     */
+    public final static double PROP_RELATIVE_BEAM_WIDTH_DEFAULT = 0.0;
+
+    /**
+     * Property that indicates whether or not the active list will
+     * implement 'strict pruning'.  When strict pruning is enabled,
+     * the active list will not remove tokens from the active list
+     * until they have been completely scored.  If strict pruning is
+     * not enabled, tokens can be removed from the active list based
+     * upon their entry scores. The default setting is false
+     * (disabled).
+     */
+
+    public final static String PROP_STRICT_PRUNING
+	= PROP_PREFIX + "strictPruning";
+
+    /**
+     * The default for the PROP_STRICT_PRUNING property
+     */
+    public final static boolean PROP_STRICT_PRUNING_DEFAULT = true;
+
+
+    /**
+     * Adds the given token to the list, keeping track of the lowest
+     * scoring token
+     *
+     * @param token the token to add
+     */
+    public void add(Token token);
+
+    /**
+     * Replaces an old token with a new token
+     *
+     * @param oldToken the token to replace (or null in which case,
+     * replace works like add).
+     *
+     * @param newToken the new token to be placed in the list.
+     *
+     */
+    public void replace(Token oldToken, Token newToken);
+
+
+    /**
+     * Purges the active list of excess members returning a
+     * (potentially new) active list
+     *
+     * @return a purged active list
+     */
+    public ActiveList purge();
+
+    /**
+     * Creates a new version of the active list with
+     * the same general properties of this one
+     *
+     * @return a new active list
+     */
+    public ActiveList createNew();
+
+
+    /**
+     * Returns the SphinxProperties of this list.
+     *
+     * @return the SphinxProperties of this list.
+     */
+    public SphinxProperties getProperties();
+
+
+    /**
+     * Sets the properties for this list
+     *
+     * @param props the properties for this list
+     */
+    public void setProperties(SphinxProperties props);
+
+
+    /**
+     * Sets the absolute beam width.
+     *
+     * @param absoluteBeamWidth the absolute beam width
+     */
+    public void setAbsoluteBeamWidth(int absoluteBeamWidth);
+
+
+    /**
+     * Sets the relative beam width.
+     *
+     * @param relativeBeamWidth the linear relative beam width
+     */
+    public void setRelativeBeamWidth(double relativeBeamWidth);
+
+
+    /**
+     * Returns an iterator over the elements in this active list
+     * 
+     * @return an iterator
+     */
+    public Iterator iterator();
+
+    /**
+     * Returns the size of this list
+     * 
+     * @return the size
+     */
+    public int size();
+
+
+    /**
+     * Gets the list of all tokens
+     *
+     * @return the set of tokens
+     */
+    public List getTokens();
+
+
+    /**
+     * gets the beam threshold best upon the best scoring token
+     *
+     * @return the beam threshold
+     */
+    public float getBeamThreshold();
+
+    /**
+     * gets the best score in the list
+     *
+     * @return the best score
+     */
+    public float getBestScore();
+
+    /**
+     * Sets the best scoring token for this active list
+     *
+     * @param token the best scoring token
+     */
+    public void setBestToken(Token token);
+
+    /**
+     * Gets the best scoring token for this active list
+     *
+     * @return the best scoring token
+     */
+    public Token getBestToken();
+}
+
---
+++ tags/leson1_version3/sphinx4/edu/cmu/sphinx/frontend/endpoint/LevelTracker.java
@@ -0,0 +1,304 @@
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+
+package edu.cmu.sphinx.frontend.endpoint;
+
+import edu.cmu.sphinx.frontend.Audio;
+import edu.cmu.sphinx.frontend.AudioSource;
+import edu.cmu.sphinx.frontend.DataProcessor;
+import edu.cmu.sphinx.frontend.FrontEnd;
+import edu.cmu.sphinx.frontend.Signal;
+
+import edu.cmu.sphinx.util.SphinxProperties;
+import edu.cmu.sphinx.util.LogMath;
+
+import java.io.IOException;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * Implements a level tracking endpointer invented by Bent Schmidt Nielsen.
+ *
+ * <p>This endpointer is composed of three main steps.
+ * <ol>
+ * <li>classification of audio into speech and non-speech
+ * <li>inserting SPEECH_START and SPEECH_END signals around speech
+ * <li>removing non-speech regions
+ * </ol>
+ *
+ * <p>The first step, classification of audio into speech and non-speech,
+ * uses Bent Schmidt Nielsen's algorithm. Each time audio comes in, 
+ * the average signal level and the background noise level are updated,
+ * using the signal level of the current audio. If the average signal
+ * level is greater than the background noise level by a certain
+ * threshold value (configurable), then the current audio is marked
+ * as speech. Otherwise, it is marked as non-speech.
+ *
+ * <p>The second and third step of this endpointer are documented in the
+ * classes SpeechMarker and AudioFilter.
+ *
+ * @see SpeechMarker
+ * @see AudioFilter
+ */
+public class LevelTracker extends DataProcessor implements AudioEndpointer {
+
+    /**
+     * Prefix for the SphinxProperties of this class.
+     */
+    public static final String PROP_PREFIX = 
+        "edu.cmu.sphinx.frontend.endpoint.LevelTracker.";
+
+    /**
+     * The SphinxProperty specifying the endpointing frame length.
+     * This is the number of samples in each endpointed frame.
+     */
+    public static final String PROP_FRAME_LENGTH = PROP_PREFIX + "frameLength";
+
+    /**
+     * The default value of PROP_FRAME_LENGTH.
+     */
+    public static final int PROP_FRAME_LENGTH_DEFAULT = 160;
+
+    /**
+     * The SphinxProperty specifying the minimum signal level used
+     * to update the background signal level.
+     */
+    public static final String PROP_MIN_SIGNAL = PROP_PREFIX + "minSignal";
+
+    /**
+     * The default value of PROP_MIN_SIGNAL.
+     */
+    public static final double PROP_MIN_SIGNAL_DEFAULT = 0;
+
+    /**
+     * The SphinxProperty specifying the threshold. If the current signal
+     * level is greater than the background level by this threshold,
+     * then the current signal is marked as speech.
+     */
+    public static final String PROP_THRESHOLD = PROP_PREFIX + "threshold";
+
+    /**
+     * The default value of PROP_THRESHOLD.
+     */
+    public static final double PROP_THRESHOLD_DEFAULT = 10;
+
+    /**
+     * The SphinxProperty specifying the adjustment.
+     */
+    public static final String PROP_ADJUSTMENT = PROP_PREFIX + "adjustment";
+
+    /**
+     * The default value of PROP_ADJUSTMENT_DEFAULT.
+     */
+    public static final double PROP_ADJUSTMENT_DEFAULT = 0.003;
+
+    /**
+     * The SphinxProperty specifying whether to print debug messages.
+     */
+    public static final String PROP_DEBUG = PROP_PREFIX + "debug";
+
+    /**
+     * The default value of PROP_DEBUG.
+     */
+    public static final boolean PROP_DEBUG_DEFAULT = false;
+    
+
+    private boolean debug;
+    private double averageNumber = 1;
+    private double adjustment;
+    private double level;               // average signal level
+    private double background;          // background signal level
+    private double minSignal;           // minimum valid signal level
+    private double threshold;
+    private int frameLength;
+    private AudioSource predecessor;
+    private SpeechMarker speechMarker;
+    private AudioFilter filter;
+    
+
+    /**
+     * Constructs an un-initialized LevelTracker.
+     */
+    public LevelTracker() {}
+
+    /**
+     * Initializes this LevelTracker endpointer with the given name, context,
+     * and AudioSource predecessor.
+     *
+     * @param name the name of this LevelTracker
+     * @param context the context of the SphinxProperties this
+     *    LevelTracker use
+     * @param props the SphinxProperties to read properties from
+     * @param predecessor the AudioSource where this LevelTracker
+     *    gets Audio from
+     *
+     * @throws java.io.IOException
+     */
+    public void initialize(String name, String context, 
+                           SphinxProperties props,
+                           AudioSource predecessor) throws IOException {
+        super.initialize(name, context, props);
+        this.predecessor = predecessor;
+        reset();
+        setProperties();
+        speechMarker = new SpeechMarker();
+        speechMarker.initialize
+            ("SpeechMarker", getContext(), getSphinxProperties(), 
+             new SpeechClassifier());
+        filter = new AudioFilter
+            ("AudioFilter", getContext(), getSphinxProperties(),
+             speechMarker);
+    }
+
+    /**
+     * Sets the properties for this LevelTracker.
+     */
+    private void setProperties() {
+        SphinxProperties props = getSphinxProperties();
+        frameLength = props.getInt
+            (PROP_FRAME_LENGTH, PROP_FRAME_LENGTH_DEFAULT);
+        adjustment = props.getDouble(PROP_ADJUSTMENT, PROP_ADJUSTMENT_DEFAULT);
+        threshold = props.getDouble(PROP_THRESHOLD, PROP_THRESHOLD_DEFAULT);
+        minSignal = props.getDouble(PROP_MIN_SIGNAL, PROP_MIN_SIGNAL_DEFAULT);
+        debug = props.getBoolean(PROP_DEBUG, PROP_DEBUG_DEFAULT);
+    }
+
+    /**
+     * Resets this LevelTracker to a starting state.
+     */
+    private void reset() {
+        level = 0;
+        background = 100;
+    }
+
+    /**
+     * Returns the next Audio object, which are already class[Aified
+     * as speech or non-speech.
+     *
+     * @return the next Audio object, or null if none available
+     *
+     * @throws java.io.IOException if an error occurred
+     *
+     * @see Audio
+     */
+    public Audio getAudio() throws IOException {
+        return filter.getAudio();            
+    }
+
+    /**
+     * Classifies Audio into either speech or non-speech.
+     */
+    class SpeechClassifier implements AudioSource {
+
+        List outputQueue = new LinkedList();
+        
+        /**
+         * Returns the logarithm base 10 of the root mean square of the
+         * given samples.
+         *
+         * @param samples the samples
+         *
+         * @return the calculated log root mean square in log 10
+         */
+        private double logRootMeanSquare(double[] samples) {
+            assert samples.length > 0;
+            double sumOfSquares = 0.0f;
+            for (int i = 0; i < samples.length; i++) {
+                double sample = samples[i];
+                sumOfSquares += sample * sample;
+            }
+            double rootMeanSquare = Math.sqrt
+                ((double)sumOfSquares/samples.length);
+            rootMeanSquare = Math.max(rootMeanSquare, 1);
+            return (LogMath.log10((float)rootMeanSquare) * 20);
+        }
+        
+        /**
+         * Classifies the given audio frame as speech or not, and updates
+         * the endpointing parameters.
+         *
+         * @param audio the audio frame
+         */
+        private void classify(Audio audio) {
+            double current = logRootMeanSquare(audio.getSamples());
+            // System.out.println("current: " + current);
+            boolean isSpeech = false;
+            if (current >= minSignal) {
+                level = ((level*averageNumber) + current)/(averageNumber + 1);
+                if (current < background) {
+                    background = current;
+                } else {
+                    background += (current - background) * adjustment;
+                }
+                if (level < background) {
+                    level = background;
+                }
+                isSpeech = (level - background > threshold);
+            }
+            audio.setSpeech(isSpeech);
+            if (debug) {
+                String speech = "";
+                if (audio.isSpeech()) {
+                    speech = "*";
+                }
+                System.out.println("Bkg: " + background + ", level: " + level +
+                                   ", current: " + current + " " + speech);
+            }
+            outputQueue.add(audio);
+        }
+        
+        /**
+         * Returns the next Audio object.
+         *
+         * @return the next Audio object, or null if none available
+         *
+         * @throws java.io.IOException if an error occurred
+         *
+         * @see Audio
+         */
+        public Audio getAudio() throws IOException {
+            if (outputQueue.size() == 0) {
+                Audio audio = predecessor.getAudio();
+                if (audio != null) {
+                    /*
+                    System.out.println("LevelTracker: incoming: " +
+                                       audio.getSignal().toString());
+                    */
+                    audio.setSpeech(false);
+                    if (audio.hasContent()) {
+                        if (audio.getSamples().length > frameLength) {
+                            throw new Error
+                                ("Size of each audio frame should be <= " +
+                                 frameLength + ". If you have 2 bytes per " +
+                                 "sample, the property " + 
+                                 FrontEnd.PROP_BYTES_PER_AUDIO_FRAME + 
+                                 "should be set to " + frameLength*2);
+                        }
+                        classify(audio);
+                    } else {
+                        outputQueue.add(audio);
+                    }
+                }
+            }
+            if (outputQueue.size() > 0) {
+                Audio audio = (Audio) outputQueue.remove(0);
+                return audio;
+            } else {
+                return null;
+            }
+        }
+    }
+}
