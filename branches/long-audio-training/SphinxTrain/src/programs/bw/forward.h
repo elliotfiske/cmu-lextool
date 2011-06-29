5b71c6f6bf4f439f7690c7c7773a99e581634091
xkrajnan <xkrajnan@94700074-3cef-4d97-a70e-9c8c206c02f5>
2011-06-29 17:19:49 +0000

    Local forward.

--- branches/long-audio-training/SphinxTrain/src/programs/bw/forward.h
+++ branches/long-audio-training/SphinxTrain/src/programs/bw/forward.h
@@ -73,6 +73,23 @@ forward(float64 **active_alpha,
 	s3phseg_t *phseg,
 	uint32 mmi_train);
 
+int32
+forward_local(float64 **active_alpha,
+	uint32 **active_astate,
+	uint32 *n_active_astate,
+	uint32 **bp,
+	float64 *scale,
+	float64 **dscale,
+	vector_t **feature,
+	uint32 n_obs,
+	state_t *state_seq,
+	uint32 n_state,
+	model_inventory_t *inv,
+	float64 beam,
+	s3phseg_t *phseg,
+	uint32 mmi_train,
+	uint32 t_offset);
+
 void
 forward_set_viterbi(int state);
 
