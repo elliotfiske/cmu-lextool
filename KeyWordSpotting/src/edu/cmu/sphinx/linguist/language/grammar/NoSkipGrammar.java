3414a6703982eb71177592c1ea513158083a23f0
apurvtiwari <apurvtiwari@94700074-3cef-4d97-a70e-9c8c206c02f5>
2011-07-12 03:53:16 +0000

    Creating KeyWord Spotting sub-branch

---
+++ KeyWordSpotting/src/edu/cmu/sphinx/linguist/language/grammar/NoSkipGrammar.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 1999-2004 Carnegie Mellon University.
+ * Portions Copyright 2004 Sun Microsystems, Inc.
+ * Portions Copyright 2004 Mitsubishi Electric Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ *
+ */
+package edu.cmu.sphinx.linguist.language.grammar;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import edu.cmu.sphinx.decoder.ResultListener;
+import edu.cmu.sphinx.linguist.dictionary.Dictionary;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.LogMath;
+import edu.cmu.sphinx.util.props.S4Component;
+
+public class NoSkipGrammar extends Grammar implements ResultListener{
+	
+	@S4Component(type = LogMath.class)
+    public final static String PROP_LOG_MATH = "logMath";
+    
+    protected GrammarNode finalNode;
+    private LogMath logMath;
+
+    private final List<String> tokens = new ArrayList<String>();
+
+    private int start;
+	public NoSkipGrammar() {
+		
+	}
+	public NoSkipGrammar(final String text,
+			final LogMath logMath, final boolean showGrammar,
+			final boolean optimizeGrammar, final boolean addSilenceWords, 
+			final boolean addFillerWords, final Dictionary dictionary) {
+		super(showGrammar, optimizeGrammar, addSilenceWords, addFillerWords, dictionary);
+        this.logMath = logMath;
+        setText(text);
+	}
+	
+	public void setText(String text) {
+		StringTokenizer st = new StringTokenizer(text);
+		while(st.hasMoreTokens()){
+			String token = st.nextToken();
+			token = token.toLowerCase();
+			while(token.compareTo(" ")!= 0){
+				tokens.add(token);
+			}			
+		}
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * We want a very strict grammar structure like the following:
+	 * InitialNode ----> KW1 ---> KW2 .... ---> KWn ---> FinalNode
+	 *   ↑________________________________________________|
+	 */
+	protected GrammarNode createGrammar(){
+		
+		initialNode = createGrammarNode(Dictionary.SILENCE_SPELLING);
+		GrammarNode branchNode = createGrammarNode(Dictionary.SILENCE_SPELLING);
+		
+		// Create grammar loop from branchNode to intial.
+		branchNode.add(initialNode, logMath.getLogOne());
+		Iterator<String> iter = tokens.iterator();
+		GrammarNode prevWordNode = initialNode;
+		GrammarNode lastWordNode = initialNode;
+		while(iter.hasNext()){
+			String currTok = iter.next();
+			lastWordNode = createGrammarNode(currTok);
+			prevWordNode.add(lastWordNode, logMath.getLogOne());
+			prevWordNode = lastWordNode;
+		}
+		lastWordNode.add(branchNode, logMath.getLogOne());
+		finalNode = createGrammarNode(Dictionary.SILENCE_SPELLING);
+		finalNode.setFinalNode(true);
+		
+		// Currently Arbitrarily setting low To-final prob.
+		// This is subject to experimentation
+		branchNode.add(finalNode, logMath.linearToLog(0.001));
+		return finalNode;		
+	}
+	@Override
+	public void newResult(Result result) {
+		return ;		
+	}
+	
+}
