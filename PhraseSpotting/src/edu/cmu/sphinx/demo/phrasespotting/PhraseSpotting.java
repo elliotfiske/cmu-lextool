cee8261fcf0c22063db83641fd8e4654a78ef0f2
apurvtiwari <apurvtiwari@94700074-3cef-4d97-a70e-9c8c206c02f5>
2011-07-28 19:17:33 +0000

    Phrase spotter demo to check the spotting accuracy

--- PhraseSpotting/src/edu/cmu/sphinx/demo/phrasespotting/PhraseSpotting.java
+++ PhraseSpotting/src/edu/cmu/sphinx/demo/phrasespotting/PhraseSpotting.java
@@ -12,21 +12,107 @@
 
 package edu.cmu.sphinx.demo.phrasespotting;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.StringTokenizer;
 
+import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
+import edu.cmu.sphinx.linguist.aflat.AFlatLinguist;
+import edu.cmu.sphinx.linguist.language.grammar.AlignerGrammar;
 import edu.cmu.sphinx.phrasespotter.Result;
 import edu.cmu.sphinx.phrasespotter.simplephrasespotter.SimplePhraseSpotter;
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.util.StringCustomise;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
 
 public class PhraseSpotting {
-	public static void main(String Args[]) throws MalformedURLException{
+	public static void main(String Args[]) throws IOException{
 		
-		SimplePhraseSpotter spotter = new SimplePhraseSpotter("./src/config.xml");
-		String phrase = "buck was";
+		final String phrase = "quantum number";
+		System.out.println("Phrase: " + phrase);
+		ConfigurationManager cm = new ConfigurationManager("./src/config.xml");
+		Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+		AFlatLinguist aflatLinguist = (AFlatLinguist) cm.lookup("linguist");
+		AlignerGrammar grammar = (AlignerGrammar) cm.lookup("grammar");
+		AudioFileDataSource dataSource = (AudioFileDataSource) cm.lookup("audioFileDataSource");
+		
+		BufferedReader reader = new BufferedReader(
+				new FileReader("./resource/Transcription/test.txt"));
+		String input = "";
+		String line ;
+		while ((line = reader.readLine()) != null) {
+			input = input.concat(line + " ");
+		}
+		StringCustomise sc = new StringCustomise();
+		input = sc.customise(input);
+		grammar.setGrammarType("");
+		grammar.setText(input);
+		dataSource.setAudioFile(new URL("file:./resource/wav/test.wav"), null);
+		recognizer.allocate();
+		
+		System.out.println("---------------- Generating Timed Result ------------------");
+		edu.cmu.sphinx.result.Result baseResult = recognizer.recognize();
+		String timedResult = baseResult.getTimedBestResult(false, true);
+		System.out.println(timedResult);
+		
+		List<String> wordsInPhrase = new LinkedList<String>();
+		StringTokenizer tokenizer = new StringTokenizer(phrase);
+		while(tokenizer.hasMoreTokens()) {
+			wordsInPhrase.add(tokenizer.nextToken());
+		}
+		
+		List<Result> baseTimedResult = new LinkedList<Result>();
+		StringTokenizer stoken = new StringTokenizer(timedResult);
+		while(stoken.hasMoreTokens()) {
+			String wordFormed = "";
+			String currWord = stoken.nextToken();
+			String word = currWord.substring(0, currWord.indexOf("("));
+			String timedPart = currWord.substring(
+					currWord.indexOf("(") + 1, currWord.indexOf(")"));
+			String st = timedPart.substring(0, timedPart.indexOf(","));
+			String et = timedPart.substring(timedPart.indexOf(",") + 1);
+			ListIterator<String> iter = wordsInPhrase.listIterator();
+			String phraseWord = iter.next();
+			float startTime = Float.valueOf(st);
+			float endTime = Float.valueOf(et);
+			int match = 0;
+			while(phraseWord.compareToIgnoreCase(word) == 0 && match < wordsInPhrase.size()){
+				match++;
+				wordFormed += word + " ";
+				endTime = Float.valueOf(et);
+				if(stoken.hasMoreTokens()){
+					if(iter.hasNext()) {
+						phraseWord = iter.next();
+						currWord = stoken.nextToken();
+						word = currWord.substring(0, currWord.indexOf("("));
+						timedPart = currWord.substring(
+								currWord.indexOf("(") + 1, currWord.indexOf(")"));
+						st = timedPart.substring(0, timedPart.indexOf(","));
+						et = timedPart.substring(timedPart.indexOf(",") + 1);
+					}
+				}
+			}			
+			// if phrase was completely detected then mark this position
+			
+			if(match == wordsInPhrase.size()) {
+				System.out.println("(" + startTime + "," + endTime + ")");
+				baseTimedResult.add(new Result(phrase, startTime, endTime));
+			}
+		}		
+		
+		// Start PhraseSpotting now and see how well it performs
+		
+		System.out.println("\n------------- Generating Phrase Spotter's Result --------------------");
+		SimplePhraseSpotter spotter = new SimplePhraseSpotter("./src/phraseSpotterConfig.xml");		
 		spotter.setPhrase(phrase);
-		spotter.setAudioDataSource(new URL("file:./resource/wav/call_of_the_wild_chapter_01.wav"));
+		spotter.setAudioDataSource(new URL("file:./resource/wav/test.wav"));
 		spotter.allocate();
 		spotter.startSpotting();
 		List<Result> result = spotter.getTimedResult();
