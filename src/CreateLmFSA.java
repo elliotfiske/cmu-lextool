991c8729128b4f11292f9909f721afa31ff8bc69
tomy91 <tomy91@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-06-30 15:27:23 +0000

    Added classes for separate FSA creation

--- src/CreateLmFSA.java
+++ src/CreateLmFSA.java
@@ -6,10 +6,10 @@ public class CreateLmFSA {
 	public static void main (String[] args) throws IOException {
 		
 		LanguageModelFSA lmFSA = new LanguageModelFSA(new URL("file:"+ args[0]), 
-				new URL("file:../models/lm_giga_5k_nvp.sphinx.dic"), 
-				new URL("file:../models/lm_giga_5k_nvp.sphinx.filler" ));
+				new URL("file:models/lm_giga_5k_nvp.sphinx.dic"), 
+				new URL("file:models/lm_giga_5k_nvp.sphinx.filler" ));
 		
-		lmFSA.writeToFile(args[1]);
+		lmFSA.writeToFile(args[1] + ".fst.txt");
 
 		lmFSA.writeSymbolsToFile(args[1] + "_isyms", args[1] + "_ssyms");
 		
