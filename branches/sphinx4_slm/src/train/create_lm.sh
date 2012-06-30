b8240ce52bdf5223900a22c5ab561caa3da7c5c2
wcluo <wcluo@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-06-30 18:29:14 +0000

    update create_lm.sh

--- branches/sphinx4_slm/src/train/create_lm.sh
+++ branches/sphinx4_slm/src/train/create_lm.sh
@@ -2,22 +2,22 @@
 #create a Language model using CMULM tool from wikipedia data
 
 Task='wiki'											#The task name
-V='65k'													#vocabulary size, "$Task-$V" will be the output language model name
-dV=65000
+V='10k'													#vocabulary size, "$Task-$V" will be the output language model name
+dV=10000
 Train='../../data/wikipedia/rnnlm/train2'		#train data file path
 Test='../../data/wikipedia/rnnlm/test'			#test data file path, use to get the perplexity
 outdir='../../data/wikipedia'
-cutoff_2=2												#cutoff for bigram
-cutoff_3=2												#cutoff for trigram
+cutoff_2=0												#cutoff for bigram
+cutoff_3=1												#cutoff for trigram
 
 # Create vocabulary
-#cat $Train | text2wfreq | wfreq2vocab -top $dV > $outdir/$Task-$V.vocab
+cat $Train | text2wfreq | wfreq2vocab -top $dV > $outdir/$Task-$V.vocab
 
 # Create trigram
-#cat $Train | text2idngram -vocab $outdir/$Task-$V.vocab > $outdir/$Task-$V.idngram
+cat $Train | text2idngram -vocab $outdir/$Task-$V.vocab > $outdir/$Task-$V.idngram
 
 # Convert to binary LM
-#idngram2lm -idngram $outdir/$Task-$V.idngram -vocab $outdir/$Task-$V.vocab -arpa $outdir/$Task-$V-$cutoff_2$cutoff_3.arpa -calc_mem -cutoffs $cutoff_2 $cutoff_3
+idngram2lm -idngram $outdir/$Task-$V.idngram -vocab $outdir/$Task-$V.vocab -arpa $outdir/$Task-$V-$cutoff_2$cutoff_3.arpa -calc_mem -cutoffs $cutoff_2 $cutoff_3
 
 #rm $outdir/$Task-$V.idngram
 
@@ -27,4 +27,4 @@ echo "LM: $outdir/$Task-$V-$cutoff_2$cutoff_3.arpa" >> log.txt
 echo "perplexity -text $Test" | evallm -arpa $outdir/$Task-$V-$cutoff_2$cutoff_3.arpa >> log.txt
 
 #convert to DMP form, doesn't work now
-# sphinx_lm_convert -i $outdir/$Task-$V-$cutoff_2$cutoff_3.arpa -o $outdir/$Task-$V-$cutoff_2$cutoff_3.lm.DMP
+sphinx_lm_convert -i $outdir/$Task-$V-$cutoff_2$cutoff_3.arpa -o $outdir/$Task-$V-$cutoff_2$cutoff_3.lm.DMP
