8794806f1bf7704792771f7d7c2f784f1c1b5847
tomy91 <tomy91@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-06-30 15:27:23 +0000

    Added classes for separate FSA creation

--- branches/ppf/src/CreateHyperstringFSA.java
+++ branches/ppf/src/CreateHyperstringFSA.java
@@ -9,6 +9,7 @@ public class CreateHyperstringFSA {
 		FSA hyperstringFSA = new FSA(args[0], true);
 		hyperstringFSA.writeToFile(args[1]);
 		hyperstringFSA.writeSymbolsToFile(args[1] + "_isyms", args[1] + "_ssyms");
+		
 	}
 	
 }
