a1b7ec5808a57e9e7d4fa8a172178e9f43338980
ronanki <ronanki@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-07-01 17:52:01 +0000

    deleting neighborphones_decode

--- ronanki/scripts/neighborphones_decode/decode.sh
+++
@@ -1,7 +0,0 @@
-SPHINX3=/home/ronanki/web/source/sphinx3/bin/sphinx3_decode
-HMM=../test/GSoC_test/wsj_all_cd30.mllt_cd_cont_4000
-
-$SPHINX3 -hmm ${HMM} -fsg phrase1.fsg -dict phone.dic -fdict phone.filler -ctl single_phone.ctl -cepdir feats -hyp single_phone.out -mode allphone -op_mode 2
-
-
-
