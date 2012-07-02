534503595304c3fdb374092b5a531900fd0c297b
wcluo <wcluo@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-07-02 17:06:23 +0000

    Code to convert mp3 to wav with 8khz sampling

--- branches/sphinx4_slm/src/train/mp2wav.sh
+++ branches/sphinx4_slm/src/train/mp2wav.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#convert mp3 to wav using three different methods: mplayer, mpg123, ffmpeg
+#convert mp3 to wav using three different methods: mplayer, mpg123, ffmpeg, libsox-fmt-all
 #you need to install these tools before run it
 
 path="../../data/IEEE"
@@ -7,16 +7,20 @@ path="../../data/IEEE"
 for f in $path/*.mp3
 do
 	fn=`basename $f .mp3`
-	mplayer -really-quiet -noconsolecontrols -nojoystick -nolirc -nomouseinput -vo null -vc null -ao      		pcm:file=tmp.wav -af format=s16le,channels=1,resample=16000:0:2 $f
+	sox $f $path/$fn.sox.wav rate 8k channels 1
 	
+	#mplayer -really-quiet -noconsolecontrols -nojoystick -nolirc -nomouseinput -vo null -vc null -ao  		pcm:file=tmp.wav -af format=s16le,channels=1,resample=16000:0:2 $f
+	mplayer -really-quiet -noconsolecontrols -nojoystick -nolirc -nomouseinput -vo null -vc null -ao  		pcm:file=tmp.wav -af format=s16le,channels=1,resample=8000:0:2 $f
 	sox tmp.wav -b 16 $path/$fn.mplayer.wav
 	rm tmp.wav
 
-	mpg123 -w tmp.wav $f 
+	ffmpeg -i $f -ac 1 -ar 8000 tmp.wav
+	sox tmp.wav -b 16 $path/$fn.ffmpeg.wav
+	rm tmp.wav
+
+	mpg123 -w tmp.wav -m -r 8000 $f 
 	sox tmp.wav -b 16 $path/$fn.mpg123.wav
 	rm tmp.wav
 
-	ffmpeg -i $f tmp.wav
-	sox tmp.wav -b 16 $path/$fn.ffmpeg.wav
-	rm tmp.wav
+	
 done
