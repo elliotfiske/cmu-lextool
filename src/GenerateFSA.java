1daf51e8f52e3ddd2b90071402d78356fbc943a0
tomy91 <tomy91@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-07-03 10:27:31 +0000

    Added usage

--- src/GenerateFSA.java
+++ src/GenerateFSA.java
@@ -3,7 +3,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 
 import edu.cmu.sphinx.linguist.dictionary.Word;
@@ -90,16 +89,28 @@ public class GenerateFSA {
 	}
 	
 	public static void main (String[] args) throws IOException {
+				
+		String text = null, lm_path = null, output_path = null;
 		
-		FSA hyperstringFSA = new FSA(args[0], true);
-		hyperstringFSA.writeToFile(args[1] + ".fst.txt");
-		//hyperstringFSA.writeSymbolsToFile(args[1] + "_isyms", args[1] + "_ssyms");
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-text")) text = args[i+1];
+			else if (args[i].equals("-path")) output_path = args[i+1];
+			else if (args[i].equals("-lm")) lm_path = args[i+1];
+		}
 		
-		LanguageModelFSA lmFSA = new LanguageModelFSA(new URL("file:"+ args[2]), 
-				new URL("file:models/lm_giga_5k_nvp.sphinx.dic"), 
-				new URL("file:models/lm_giga_5k_nvp.sphinx.filler" ));
+		if (args.length < 6 || text == null || output_path == null || lm_path == null) {
+			System.out.println("Usage: sh ./generateFST.sh -text t -lm lm_path -path output_path");
+			return;
+		}
 		
-		lmFSA.writeToFile(args[3] + ".fst.txt");
+		FSA hyperstringFSA = new FSA(text, true);
+		hyperstringFSA.writeToFile(output_path + "/hypestring.fst.txt");
+		
+		LanguageModelFSA lmFSA = new LanguageModelFSA(new URL("file:"+ lm_path), 
+				new URL("file:../models/lm_giga_5k_nvp.sphinx.dic"), 
+				new URL("file:../models/lm_giga_5k_nvp.sphinx.filler" ));
+		
+		lmFSA.writeToFile(output_path + "/lm.fst.txt");
 		
 		LinkedList<State> hyperstringStates = hyperstringFSA.getStates();
 		LinkedList<State> lmStates = lmFSA.getStates();
@@ -107,6 +118,6 @@ public class GenerateFSA {
 		states = concatenateLists(hyperstringStates, lmStates);
 		symbolSet = uniteSets(hyperstringFSA.getSymbols(), lmFSA.getSymbols());
 		
-		writeSymbols("../testdir/isyms", "../testdir/ssyms");
+		writeSymbols(output_path + "/isyms", output_path + "/ssyms");
 	}
 }
