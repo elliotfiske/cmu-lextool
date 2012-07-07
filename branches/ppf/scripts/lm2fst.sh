bb15ab72a682a557cf2975d4e347e600f42bc2ab
tomy91 <tomy91@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-07-07 10:41:33 +0000

    minor changes to scripts

--- branches/ppf/scripts/lm2fst.sh
+++ branches/ppf/scripts/lm2fst.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
 
-lm_path=$1
-fst_path=$2
-
-java -classpath ../lib/commons-lang-2.0.jar:../lib/sphinx4.jar:../bin/ CreateLmFSA $lm_path $fst_path
+java -classpath ../lib/commons-lang-2.0.jar:../lib/sphinx4.jar:../bin/ CreateLmFSA "$1" "$2" "$3" "$4"
