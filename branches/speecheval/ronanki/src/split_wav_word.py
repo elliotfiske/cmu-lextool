ac0b11f0a083f5b88c0dee1f367f0fd4566ddcf1
ronanki <ronanki@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-08-20 19:27:12 +0000

    Final GSoC submission for Pronunciation Evaluation project

--- branches/speecheval/ronanki/src/split_wav_word.py
+++ branches/speecheval/ronanki/src/split_wav_word.py
@@ -3,23 +3,18 @@ import sys
 
 
 if len(sys.argv) != 5:
-        print '\n Usage: python split_wav_word.py <input_phoneseg_file> <nbg_key_mapper> <input_wav_file> <out_split_dir> \n'
+        print '\n Usage: python split_wav_word.py <input_phoneseg_file> <CMUphone_list> <input_wav_file> <out_split_dir> \n'
         sys.exit(1)
 
 phonelist = open(sys.argv[1], "r");
-in_key = open(sys.argv[2], "r");
+in_phone = open(sys.argv[2], "r");
 
-cmuphones = {}
 allphones = []
+cmuphones = {}
 
-while True:
-	p1 = in_key.readline();
-	key_phone = p1.strip()
-	if key_phone =='':
-		break;
-	s = key_phone.split()
-	if s[0] not in cmuphones:
-		cmuphones[s[0]]=s[2]
+for i in in_phone.readlines():
+	phn = i.strip()
+	cmuphones[phn]=phn;
 
 for p1 in phonelist.readlines():
 	seg_p1 = p1.strip()
@@ -30,40 +25,21 @@ for p1 in phonelist.readlines():
 
 j=0;
 wc=0;
-k=0;
+st_ind=0;
 while 1:
 	if(j==len(allphones)):
 		break;
 	else:
-		if(allphones[j][3]=='e'):
+		if(allphones[j][3]=='e' or allphones[j][3]=='s'):
 			wc = wc + 1
 			print "word count : "+str(wc)
 		else:
 			j=j+1;
-			print "middle of the word"
 			continue;
-	st_ind = k;
-	fn_ind = j;
+	fn_ind=j;
 	ph_start =  (((float(allphones[st_ind][0])-1)*10)+1)/1000;
 	ph_end = ((float(allphones[fn_ind][1])*10)+15)/1000 - ph_start;
-	while 1:
-		if(k>j):
-			break;
-		z = st_ind;
-		count = fn_ind;
-		phlist=''
-		while(z<=count):
-			if(z==k):
-				phlist = phlist+' ('+cmuphones[allphones[z][2]]+')'
-			else:
-				phlist = phlist+' ('+allphones[z][2]+')'
-			z=z+1;
-		s_id = "%.2d" % (k+1)
-		fname = str(s_id)+'_'+allphones[k][2]
-		os.system('sox '+sys.argv[3]+' '+sys.argv[4]+fname+'.wav trim '+str(ph_start)+' '+str(ph_end));
-		ph_fname = 'FSG_words/JSGF/'+fname+'.jsgf'
-		ph = open(ph_fname, "w");
-		ph.write('#JSGF V1.0;\n\ngrammar phonelist;\n\npublic <phonelist> = ('+phlist+');\n\n');
-		k = k + 1;
-
-	j=j+1;
+	fname = 'word_'+str(wc)
+	os.system('sox '+sys.argv[3]+' '+sys.argv[4]+fname+'.wav trim '+str(ph_start)+' '+str(ph_end));
+	j=j+1
+	st_ind=j;
