8bfdabd7d17f961c5898e3a8402a1dcd515decdf
bic-user <bic-user@94700074-3cef-4d97-a70e-9c8c206c02f5>
2013-11-06 18:57:45 +0000

    adseg and fileseg merged, end_sil counting refactored

--- branches/feature-vad/sphinxbase/src/sphinx_adtools/cont_seg.c
+++ branches/feature-vad/sphinxbase/src/sphinx_adtools/cont_seg.c
@@ -35,14 +35,12 @@
  *
  */
 /*
- * cont_adseg.c -- Continuously listen and segment input speech into utterances.
+ * cont_seg.c -- Continuously listen and segment input speech into utterances.
  * 
  * HISTORY
  *
- * 02-Nov-13    Refactored to use new snr-based VAD
+ * 05-Nov-13    Created from adseg and fileseg
  * 
- * 27-Jun-96	M K Ravishankar (rkm@cs.cmu.edu) at Carnegie Mellon University
- * 		Created.
  */
 
 #include <stdio.h>
@@ -58,6 +56,10 @@
 #include <sphinxbase/ckd_alloc.h>
 #include <sphinxbase/err.h>
 
+//how many times read_audio should return 0
+//to terminate reading/recording
+#define READ_ATTEMPTS 100
+
 static const arg_t cont_args_def[] = {
     waveform_to_cepstral_command_line_macro(),
     /* Argument file. */
@@ -69,6 +71,10 @@ static const arg_t cont_args_def[] = {
      ARG_STRING,
      NULL,
      "Name of audio device to use for input."},
+	{"-infile",
+     ARG_STRING,
+     NULL,
+     "Name of audio file to use for input."},
     {"-end_sil_len",
      ARG_FLOAT32,
      "1.0",
@@ -78,6 +84,9 @@ static const arg_t cont_args_def[] = {
 
 static fe_t* fe;
 static cmd_ln_t *config;
+static int (*read_audio)(int16 *buf, int len);
+static ad_rec_t *ad;
+static FILE *infile;
 
 /* Sleep for specified msec */
 static void
@@ -96,6 +105,27 @@ sleep_msec(int32 ms)
 #endif
 }
 
+static int 
+read_audio_file(int16 *buf, int len)
+{
+    if (!infile) {
+		E_FATAL("Failed to read audio from file\n");
+		return -1;
+	}
+	return fread(buf, sizeof(int16), len, infile);
+}
+
+static int
+read_audio_adev(int16 *buf, int len)
+{
+    if (!ad) {
+		E_FATAL("Failed to read audio from mic\n");
+		return -1;
+	}
+	sleep_msec(50);
+	return ad_read(ad, buf, len);
+}
+
 /*
  * @brief Buffer for prespeech audio storage.
  *        VAD in fe triggers only after N speech frames,
@@ -105,9 +135,16 @@ sleep_msec(int32 ms)
  */
 typedef struct prespch_buf_s {
     int16 **buf;
+	/* write pointer 
+	   in frames */
 	int write_ptr;
+	/* amount of frames */
 	int frame_num;
+	/* frame len in samples */
 	int frame_len;
+	/* total amount of frames 
+	   written into prespeech buf */
+	int frame_num_total;
 } prespch_buf_t;
 
 prespch_buf_t*
@@ -119,6 +156,7 @@ prespch_buf_init(int frame_num, int frame_len)
 	prespch_buf->frame_num = frame_num;
 	prespch_buf->frame_len = frame_len;
 	prespch_buf->write_ptr = 0;
+	prespch_buf->frame_num_total = 0;
 	return prespch_buf;
 }
 
@@ -131,14 +169,23 @@ prespch_buf_free(prespch_buf_t* prespch_buf)
 	    
 }
 
+/**
+ * @brief write audio data into prespch_buf
+ * @param [in/out] in - buffer to write, 
+                   out - buffer that was substituted
+ */
 void
 prespch_buf_write(prespch_buf_t *prespch_buf, int16* in_buf, int len)
 {
+	int16 tmp_buf[512];
+	memcpy(tmp_buf, prespch_buf->buf[prespch_buf->write_ptr], sizeof(int16)*len);
     memcpy(prespch_buf->buf[prespch_buf->write_ptr], in_buf, sizeof(int16)*len);
+	memcpy(in_buf, tmp_buf, sizeof(int16)*len);
 	prespch_buf->write_ptr++;
 	if (prespch_buf->write_ptr >= prespch_buf->frame_num)
 	    //start writing from the beginning
 		prespch_buf->write_ptr = 0;
+	    prespch_buf->frame_num_total++;
 }
 
 void
@@ -151,14 +198,25 @@ prespch_buf_dump(prespch_buf_t *prespch_buf, FILE *fp)
 	for (i=0; i<prespch_buf->write_ptr; i++)
 	    fwrite(prespch_buf->buf[i], sizeof(int16), prespch_buf->frame_len, fp);
 	prespch_buf->write_ptr = 0;
-}   /* end of prespch_buf declaration */
+	prespch_buf->frame_num_total = 0;
+}   
+
+int
+prespch_buf_get_nframes(prespch_buf_t *prespch_buf)
+{
+	return prespch_buf->frame_num_total;
+}/* end of prespch_buf declaration */
 
 /*
  * @brief Buffer for frame accumulating
  */
 typedef struct frame_buf_s {
 	int16 *buf;
+	/* write pointer 
+	   in samples */
 	int write_ptr;
+	/* read pointer 
+	   in samples */
 	int read_ptr;
 } frame_buf_t;
 
@@ -211,29 +269,31 @@ frame_buf_get_len(frame_buf_t *frame_buf)
 } /* end of frame_buf declaration */
 
 /*
- * Segment raw A/D input data into utterances whenever silence region of given
+ * Segment input audio data into utterances whenever silence region of given
  * duration is encountered.
  * Utterances are written to files named 0001.raw, 0002.raw, 0003.raw, etc.
  */
 void
-record_segments()
+segment_audio()
 {
-    ad_rec_t *ad;
 	frame_buf_t *frame_buf;
 	prespch_buf_t *prespch_buf;
 	FILE *fp;
 	int16 *frame;
 	
-	int i, k, uttno, uttlen, sample_rate;
-	int start_sil_num;
+	int k, uttno, uttlen, sample_rate, read_attempts;
+	int start_sil_num, end_sil_num;
 	int frame_len, frame_overlap;
 	uint8 vad_state, vad_prev_state, is_writing;
+	uint8 finish_reading;
 	
-    int16 ad_buf[2048];
+    int16 raw_buf[2048];
 	mfcc_t mfcc_buf[128];
     char file[1024];
 	
     start_sil_num = cmd_ln_int_r(config, "-vad_prespeech");
+	end_sil_num = (int)(cmd_ln_float_r(config, "-end_sil_len") * 
+		cmd_ln_int_r(config, "-frate"));
 	sample_rate = (int)cmd_ln_float32_r(config, "-samprate");
 	frame_len = cmd_ln_float32_r(config, "-wlen") * sample_rate;
 	frame_overlap = sample_rate/cmd_ln_int_r(config, "-frate");
@@ -241,40 +301,39 @@ record_segments()
 	frame_buf = frame_buf_init(4096);
 	prespch_buf = prespch_buf_init(start_sil_num, frame_overlap);
 	
-    /* Open raw A/D device */
-    if ((ad = ad_open_dev(cmd_ln_str_r(config, "-adcdev"),sample_rate)) == NULL)
-        E_FATAL("Failed to open audio device\n");
-
-    printf("Start recording ...\n");
-    fflush(stdout);
-    if (ad_start_rec(ad) < 0)
-        E_FATAL("Failed to start recording\n");
-	/* skip empty buffers */
-	for (i=0; i<5; i++) {
-		sleep_msec(200);
-		k=ad_read(ad, ad_buf, 2048);
-	}
-    
     /* Forever listen for utterances */
     uttno = 0;
 	uttlen = 0;
 	vad_state = 0;
 	vad_prev_state = 0;
 	is_writing = 0;
+	finish_reading = 0;
+	read_attempts = 0;
 	fp = NULL;
 	if (fe_start_utt(fe) < 0)
 	    E_FATAL("Failed to start utterance\n");
-	printf("You may speak now\n");
-    fflush(stdout);
     for (;;) {
 
         /* filling frame buffer, to produce at least one frame */
         while (frame_buf_get_len(frame_buf) < frame_len) {
-			if ((k = ad_read(ad, ad_buf, 2048)) < 0)
+			if ((k = read_audio(raw_buf, 2048)) < 0)
 				E_FATAL("Failed to read audio\n");
-			frame_buf_write(frame_buf, ad_buf, k);
+			if (k == 0) {
+			    read_attempts++;
+				if (read_attempts >= READ_ATTEMPTS) {
+					//several attempts to read audio failed
+					finish_reading = 1;
+					break;
+				}
+			} else {
+				read_attempts = 0;
+			}
+			frame_buf_write(frame_buf, raw_buf, k);
 		}
 		
+		if (finish_reading)
+			break;
+
 		/* using obtained audio while there is at least frame */
 		while (frame_buf_get_len(frame_buf) > frame_len) {
 		    frame_buf_read(frame_buf, frame, frame_len, frame_overlap);
@@ -283,27 +342,39 @@ record_segments()
 						
 			if (vad_state) {
 			    if (!vad_prev_state) {
-				    //utterance detected. file should be created, prespeech dumped
-					uttno++;
-                    sprintf(file, "%04d.raw", uttno);
-                    if ((fp = fopen(file, "wb")) == NULL)
-                        E_FATAL_SYSTEM("Failed to open '%s' for reading", file);
+				    //utterance detected
+					if (!is_writing) {
+					    //creating file for speech segment
+						uttno++;
+                        sprintf(file, "%04d.raw", uttno);
+                        if ((fp = fopen(file, "wb")) == NULL)
+                            E_FATAL_SYSTEM("Failed to open '%s' for reading", file);
+					    printf("Utterance %04d, logging to %s\n", uttno, file);
+					} //in other case end sil specified by user hasn't ended, 
+					  //so just continue writing
                     prespch_buf_dump(prespch_buf, fp);
-					uttlen = start_sil_num * frame_overlap;
-					printf("Utterance %04d, logging to %s\n", uttno, file);
+					uttlen += start_sil_num * frame_overlap;
 					is_writing = 1;
 				}
 				//we're in the utterance, just dump frame to file
 				fwrite(frame, sizeof(*frame), frame_overlap, fp);
 				uttlen += frame_overlap;
 			} else {
-			    prespch_buf_write(prespch_buf, frame, frame_overlap);
+				prespch_buf_write(prespch_buf, frame, frame_overlap);
 				if (is_writing) {
-				     //end of utterance detected. File should be closed
-					 fclose(fp);
-                     printf("\tUtterance %04d = %d samples (%.1fsec)\n\n",
+				    if (prespch_buf_get_nframes(prespch_buf) > start_sil_num) {
+					    //end silence specified by user isn't end yet
+						fwrite(frame, sizeof(*frame), frame_overlap, fp);
+						uttlen += frame_overlap;
+					}
+					if (prespch_buf_get_nframes(prespch_buf) > end_sil_num) {
+					    //end of utterance detected. File should be closed
+					    fclose(fp);
+                        printf("\tUtterance %04d = %d samples (%.1fsec)\n\n",
                         uttno, uttlen, (double) uttlen / (double) sample_rate);
-					is_writing = 0;
+					    is_writing = 0;
+						uttlen = 0;
+				    }
 				}
 			}
 			vad_prev_state = vad_state;
@@ -317,8 +388,6 @@ record_segments()
                 uttno, uttlen, (double) uttlen / (double) sample_rate);
 	}
 	
-    ad_stop_rec(ad);
-    ad_close(ad);
 	fe_end_utt(fe, mfcc_buf, &frame_len);
 	if (frame)
 	    ckd_free((void*)frame);
@@ -331,8 +400,9 @@ record_segments()
 int
 main(int argc, char *argv[])
 {
-    char const *cfg;
-	int end_sil_num;
+    char const *cfg, *infile_path;
+	int i;
+	int16 buf[2048];
 	
     if (argc == 2) {
         config = cmd_ln_parse_file_r(NULL, cont_args_def, argv[1], TRUE);
@@ -346,15 +416,47 @@ main(int argc, char *argv[])
     }
     if (config == NULL)
         return 1;
-		
-	end_sil_num = (int)(cmd_ln_float_r(config, "-end_sil_len") * cmd_ln_int_r(config, "-frate"));
-	cmd_ln_set_int_r(config, "-vad_postspeech", end_sil_num);
 	
+	if ((infile_path = cmd_ln_str_r(config, "-infile")) != NULL) {
+	    //reading audio from file
+		if ((infile = fopen(infile_path, "rb")) == NULL) {
+			E_FATAL("Unable to read audio from [%s]\n", infile_path);
+			return 1;
+		}
+		read_audio = &read_audio_file;
+		//skip wav header
+		read_audio(buf, 44);
+	} else {
+		//readinf audio from A/D device
+        if ((ad = ad_open_dev(cmd_ln_str_r(config, "-adcdev"),
+			(int)cmd_ln_float32_r(config, "-samprate"))) == NULL) {
+            E_FATAL("Failed to open audio device\n");
+			return 1;
+		}
+		read_audio = &read_audio_adev;
+        printf("Start recording ...\n");
+        fflush(stdout);
+        if (ad_start_rec(ad) < 0)
+           E_FATAL("Failed to start recording\n");
+	    //skip empty buffers
+	    for (i=0; i<5; i++) {
+		    sleep_msec(200);
+		    read_audio(buf, 2048);
+	    }
+		printf("You may speak now\n");
+        fflush(stdout);
+	}
+
     fe = fe_init_auto_r(config);
     if (fe == NULL)
         return 1;
 
-    record_segments();
+    segment_audio();
+
+	if (ad)
+		ad_close(ad);
+	if (infile)
+		fclose(infile);
 
     fe_free(fe);
     return 0;
