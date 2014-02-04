d55ee046b96b6c43e88d06391f0d8fd7f8a37bd6
darkseeker <darkseeker@94700074-3cef-4d97-a70e-9c8c206c02f5>
2014-02-04 15:25:23 +0000

    Added speaker diarization demo

---
+++ branches/hl-interface/src/apps/edu/cmu/sphinx/demo/diarization/SpeakerDiarizationDemo.java
@@ -0,0 +1,39 @@
+package edu.cmu.sphinx.demo.diarization;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import edu.cmu.sphinx.diarization.*;
+
+public class SpeakerDiarizationDemo {
+	/**
+	 * 
+	 * @param speakers
+	 *            An array of clusters for which it is needed to be printed the
+	 *            speakers intervals
+	 * @throws IOException
+	 */
+	public static void printSpeakerIntervals(
+			ArrayList<SpeakerCluster> speakers, String fileName)
+			throws IOException {
+		String ofName = fileName.substring(0, fileName.indexOf('.')) + ".seg";
+		FileWriter fr = new FileWriter(ofName);
+		for (int i = 0; i < speakers.size(); i++) {
+			ArrayList<Integer> t = speakers.get(i).getSpeakerIntervals();
+			for (int j = 0; j < t.size() / 2; j++)
+				fr.write(fileName + " " + 1 + " " + t.get(2 * j) + " "
+						+ t.get(2 * j + 1) + " U U U S" + i + '\n');
+		}
+		fr.close();
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		String inputFile = "src/apps/edu/cmu/sphinx/demo/diarization/librivox_community_podcast_135.wav";
+		SpeakerDiarization sd = new SpeakerDiarization();
+		ArrayList<SpeakerCluster> clusters = sd.cluster(inputFile);
+		printSpeakerIntervals(clusters, inputFile);
+	}
+
+}
