39cf6961a03b46471bc88f80abd9f8b483bc03cc
bic-user <bic-user@94700074-3cef-4d97-a70e-9c8c206c02f5>
2015-04-14 11:46:36 +0000

    read/write functions are defined in general ngram_model header

--- src/libsphinxbase/lm/ngram_model_trie.h
+++ src/libsphinxbase/lm/ngram_model_trie.h
@@ -5,12 +5,11 @@
 #include <sphinxbase/prim_type.h>
 #include <sphinxbase/logmath.h>
 
-typedef struct ngram_model_trie_s ngram_model_trie_t;
-
-void trie_write_bin(ngram_model_trie_t *model, const char *path);
-
-ngram_model_trie_t* trie_read_bin(const char *path);
-
-float trie_score(ngram_model_trie_t *model, const char*const *words, int32 n);
+typedef struct ngram_model_trie_s {
+    ngram_model_t base;  /**< Base ngram_model_t structure */
+    lm_trie_t *trie;     /**< Trie structure that stores ngram relations and weights */
+    int32 log_wip;       /**< To avoid calculating it on every score query */
+    float32 lw;          /**< Language weight to apply on scores */
+}ngram_model_trie_t;
 
 #endif /* __NGRAM_MODEL_TRIE_H__ */
