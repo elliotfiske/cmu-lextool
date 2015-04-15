7120624dae88cabc035b60ddd819f30c7f081300
bic-user <bic-user@94700074-3cef-4d97-a70e-9c8c206c02f5>
2015-04-15 14:17:54 +0000

    move from NGRAM_DMP to NGRAM_BIN enum in tests

--- branches/sphinxbase-trie/test/unit/test_ngram/test_lm_iter.c
+++ branches/sphinxbase-trie/test/unit/test_ngram/test_lm_iter.c
@@ -20,7 +20,7 @@ main(int argc, char *argv[])
 	/* Initialize a logmath object to pass to ngram_read */
 	lmath = logmath_init(1.0001, 0, 0);
 	/* Read a language model */
-	model = ngram_model_read(NULL, LMDIR "/100.arpa.DMP", NGRAM_DMP, lmath);
+	model = ngram_model_read(NULL, LMDIR "/100.arpa.DMP", NGRAM_BIN, lmath);
 	TEST_ASSERT(model);
 
 	for (i = 0, itor = ngram_model_mgrams(model, 0);
