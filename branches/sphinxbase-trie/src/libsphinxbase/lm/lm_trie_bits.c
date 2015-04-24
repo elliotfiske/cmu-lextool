7cec9092d23bb9a109dfa9e6e6ad883eacee124d
bic-user <bic-user@94700074-3cef-4d97-a70e-9c8c206c02f5>
2015-04-24 16:24:38 +0000

    trying to fix bits sanity

--- branches/sphinxbase-trie/src/libsphinxbase/lm/lm_trie_bits.c
+++ branches/sphinxbase-trie/src/libsphinxbase/lm/lm_trie_bits.c
@@ -22,17 +22,17 @@ void bit_mask_from_max(bit_mask_t *bit_mask, uint32 max_value)
 void bit_packing_sanity() {
   const float_enc neg1 = { -1.0 }, pos1 = { 1.0 };
   const uint32 test31 = 0x1234567U;
-  char mem[31+8];
+  char mem[32+8];
   uint32 b;
   if ((neg1.i ^ pos1.i) != 0x80000000) {
       E_ERROR("Sign bit is not 0x80000000\n");
   }
   memset(mem, 0, sizeof(mem));
-  for (b = 0; b < 31 * 8; b += 31) {
-    write_int31(mem, b, 31, test31);
+  for (b = 0; b < 32 * 8; b += 32) {
+    write_int31(mem, b, 32, test31);
   }
-  for (b = 0; b < 31 * 8; b += 31) {
-    if (test31 != read_int31(mem, b, 31, (1U << 31) - 1))
+  for (b = 0; b < 32 * 8; b += 32) {
+    if (test31 != read_int31(mem, b, 32, ((1ULL << 32) - 1)))
       E_ERROR("The bit packing routines are failing for your architecture.  Please send a bug report with your architecture, operating system, and compiler\n");
   }
   // TODO: more checks.  
