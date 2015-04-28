2c22b6e5b231792dbb373e227bd0ba7d8b3e64ec
bic-user <bic-user@94700074-3cef-4d97-a70e-9c8c206c02f5>
2015-04-28 10:39:07 +0000

    get rid of inlines and bulk defines in bit array

--- branches/sphinxbase-trie/src/libsphinxbase/util/bitarr.c
+++ branches/sphinxbase-trie/src/libsphinxbase/util/bitarr.c
@@ -37,10 +37,137 @@
 
 /*
  * bitarr.c -- Bit array manipulations implementation.
- * Most of functions are in defined in header to speed up things.
  */
 
+#ifdef __APPLE__
+#include <architecture/byte_order.h>
+#elif __linux__
+#include <endian.h>
+#elif !defined(_WIN32) && !defined(_WIN64)
+#include <arpa/nameser_compat.h>
+#endif 
+
 #include "sphinxbase/bitarr.h"
+
+/**
+ * Union to map float to integer to store latter in bit array
+ */
+typedef union { 
+    float f; 
+    uint32 i; 
+} float_enc;
+
+#define SIGN_BIT (0x80000000)
+
+/**
+ * Shift bits depending on byte order in system.
+ * Fun fact: __BYTE_ORDER is wrong on Solaris Sparc, but the version without __ is correct.
+ * @param bit is an offset last byte
+ * @param length - amount of bits for required for digit that is going to be read
+ * @return shift forgiven architecture
+ */
+static uint8 get_shift(uint8 bit, uint8 length)
+{
+#if BYTE_ORDER == LITTLE_ENDIAN
+    return bit;
+#elif BYTE_ORDER == BIG_ENDIAN
+    return 64 - length - bit;
+#else
+#error "Bit packing code isn't written for your byte order."
+#endif
+}
+
+/**
+ * Read uint64 value from the given adress
+ * @param adress to read from
+ * @param pointer to value where to save read value
+ * @return uint64 value that was read
+ */
+static uint64 read_off(bitarr_adress_t adress)
+{
+#if defined(__arm) || defined(__arm__)
+    uint64 value64;
+    const uint8 *base_off = (const uint8 *)(adress.base) + (adress.offset >> 3);
+    memcpy(&value64, base_off, sizeof(value64));
+    return value64;
+#else
+    return *(const uint64*)((const uint8 *)(adress.base) + (adress.offset >> 3));
+#endif
+}
+
+uint64 bitarr_read_int57(bitarr_adress_t adress, uint8 length, uint64 mask)
+{
+    return (read_off(adress) >> get_shift(adress.offset & 7, length)) & mask;
+}
+
+void bitarr_write_int57(bitarr_adress_t adress, uint8 length, uint64 value) 
+{
+#if defined(__arm) || defined(__arm__)
+    uint64 value64;
+    uint8 *base_off = (uint8 *)(adress.base) + (adress.offset >> 3);
+    memcpy(&value64, base_off, sizeof(value64));
+    value64 |= (value << get_shift(adress.offset & 7, length));
+    memcpy(base_off, &value64, sizeof(value64));
+#else
+    *(uint64 *)((uint8 *)(adress.base) + (adress.offset >> 3)) |= (value << get_shift(adress.offset & 7, length));
+#endif
+}
+
+uint32 bitarr_read_int25(bitarr_adress_t adress, uint8 length, uint32 mask) 
+{
+#if defined(__arm) || defined(__arm__)
+    uint32 value32;
+    const uint8 *base_off = (const uint8_t*)(adress.base) + (adress.offset >> 3);
+    memcpy(&value32, adress.offset, sizeof(value32));
+    return (value32 >> get_shift(adress.offset & 7, length)) & mask;
+#else
+    return (*(const uint32_t*)((const uint8_t*)(adress.base) + (adress.offset >> 3)) >> get_shift(adress.offset & 7, length)) & mask;
+#endif
+}
+
+void bitarr_write_int25(bitarr_adress_t adress, uint8 length, uint32 value)
+{
+#if defined(__arm) || defined(__arm__)
+    uint32 value32;
+    uint8 *base_off = (uint8 *)(adress.base) + (adress.offset >> 3);
+    memcpy(&value32, base_off, sizeof(value32));
+    value32 |= (value << get_shift(adress.offset & 7, length));
+    memcpy(base_off, &value32, sizeof(value32));
+#else
+    *(uint32_t *)((uint8 *)(adress.base) + (adress.offset >> 3)) |= (value << get_shift(adress.offset & 7, length));
+#endif
+}
+
+float bitarr_read_negfloat(bitarr_adress_t adress) 
+{
+    float_enc encoded;
+    encoded.i = (uint32)(read_off(adress) >> get_shift(adress.offset & 7, 31));
+    // Sign bit set means negative.  
+    encoded.i |= SIGN_BIT;
+    return encoded.f;
+}
+
+void bitarr_write_negfloat(bitarr_adress_t adress, float value) 
+{
+    float_enc encoded;
+    encoded.f = value;
+    encoded.i &= ~SIGN_BIT;
+    bitarr_write_int57(adress, 31, encoded.i);
+}
+
+float bitarr_read_float(bitarr_adress_t adress)
+{
+    float_enc encoded;
+    encoded.i = (uint32)(read_off(adress) >> get_shift(adress.offset & 7, 32));
+    return encoded.f;
+}
+
+void bitarr_write_float(bitarr_adress_t adress, float value) 
+{
+    float_enc encoded;
+    encoded.f = value;
+    bitarr_write_int57(adress, 32, encoded.i);
+}
 
 void bitarr_mask_from_max(bitarr_mask_t *bit_mask, uint32 max_value)
 {
